b1c4852fde5a392dee833f0ad3d4d34f640c6e9d
Carter Harrod <harrod@ku.edu>
2020-11-14 16:21:29 -0600

    exercise2 finished

--- exercise2/Quiz.php
+++ exercise2/Quiz.php
@@ -3,36 +3,36 @@
 $score = 0;
 
 echo "<br><p>Question 1: What is the best number?</p>";
-echo "<pre>&#9</pre>You answered: " . $_POST["bestNumber"] "<br>";
-echo "<pre>&#9</pre>Correct answer: 283";
+echo "You answered: " . $_POST["bestNumber"] . "<br>";
+echo "Correct answer: 283";
 if($_POST["bestNumber"] == 283){
 	$score += 20;
 }
 
 echo "<br><p>Question 2: Which is a fruit?</p>";
-echo "<pre>&#9</pre>You answered: " . $_POST["fruit"] "<br>";
-echo "<pre>&#9</pre>Correct answer: apple";
+echo "You answered: " . $_POST["fruit"] . "<br>";
+echo "Correct answer: apple";
 if($_POST["fruit"] == "apple"){
 	$score += 20;
 }
 
 echo "<br><p>Question 2: Which is a bird?</p>";
-echo "<pre>&#9</pre>You answered: " . $_POST["bird"] "<br>";
-echo "<pre>&#9</pre>Correct answer: bluebird";
+echo "You answered: " . $_POST["bird"] . "<br>";
+echo "Correct answer: bluebird";
 if($_POST["bird"] == "bluebird"){
 	$score += 20;
 }
 
 echo "<br><p>Question 2: Which is an insect?</p>";
-echo "<pre>&#9</pre>You answered: " . $_POST["insect"] "<br>";
-echo "<pre>&#9</pre>Correct answer: ant";
+echo "You answered: " . $_POST["insect"] . "<br>";
+echo "Correct answer: ant";
 if($_POST["insect"] == "ant"){
 	$score += 20;
 }
 
 echo "<br><p>Question 2: Which is a food?</p>";
-echo "<pre>&#9</pre>You answered: " . $_POST["food"] "<br>";
-echo "<pre>&#9</pre>Correct answer: pizza";
+echo "You answered: " . $_POST["food"] . "<br>";
+echo "Correct answer: pizza";
 if($_POST["food"] == "pizza"){
 	$score += 20;
 }
@@ -41,3 +41,4 @@ echo "<p>You got a $score% on the quiz, or " . $score / 20 . "/5 answers correct
 
 
 ?>
+
